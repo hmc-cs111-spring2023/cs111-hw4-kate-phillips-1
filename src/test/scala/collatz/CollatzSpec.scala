efea0bb2edcd6b213ef0149ba5cb3eb2a1011bb6
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-02-10 06:03:30 +0000

    writing collatz tests

--- src/test/scala/collatz/CollatzSpec.scala
+++ src/test/scala/collatz/CollatzSpec.scala
@@ -3,6 +3,29 @@ import org.scalatest.matchers.should._
 
 class CollatzSpec extends AnyFunSuite with Matchers:
 
-// write tests here!
+    test("Test 1") {
+        collatz(6) should be(3)
+    }
+
+    test("Test 2") {
+        collatz(7) should be(22)
+    }
+    test("Test 3") {
+        collatz(1) should be(4)
+    }
+
+    test("Test 4") {
+        collatz(2) should be(1)
+    }
+
+    test("Test 5") {
+        collatz(42) should be(21)
+    }
+
+    test("Test 6") {
+        collatz(101) should be(304)
+    }
+
+
 
 end CollatzSpec
